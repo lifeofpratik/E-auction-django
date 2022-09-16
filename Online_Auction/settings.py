a0a425776e8a5212f180e9b5e022980d498d9c19
Pratik Agarwal <lifeofpratik17@gmail.com>
2022-09-16 16:16:03 +0530

    ADD static dir 5

--- Online_Auction/settings.py
+++ Online_Auction/settings.py
@@ -119,8 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static/')
-STATICFILES_DIRS = (('auction',os.path.join(BASE_DIR,'auction','static')),)
+
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR,'static/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')

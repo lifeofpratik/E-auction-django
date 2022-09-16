b869b12b4ca1df437b5811d527404d09d9813cb3
Pratik Agarwal <lifeofpratik17@gmail.com>
2022-09-16 18:38:26 +0530

    ADD static dir 7

--- Online_Auction/settings.py
+++ Online_Auction/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_igtoud8=l*xejhiwtfz$ce-fu*%80&^hybem91#g#)5j7td#z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
--- Online_Auction/urls.py
+++ Online_Auction/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import url
-from django.conf import settings
-from django.views.static import serve
+#from django.conf.urls import url
+#from django.conf import settings
+#from django.views.static import serve
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
@@ -9,7 +9,7 @@ from auction.views import *
 
 urlpatterns = [
     #path('auction',include('auction.urls')),
-    url(r'^static/(?P<path>.*)$',serve,{'document_root':settings.STATIC_ROOT}),
+    #url(r'^static/(?P<path>.*)$',serve,{'document_root':settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
     path('',Home,name="home"),
     path('user_home',Bidder_Home,name="user_home"),
